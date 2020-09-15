5dd95c17371f2ada6fec5fe4e71159ed4c37a43c
evengul <even.gultvedt@gmail.com>
2020-09-15 14:04:28 +0200

    Fullført b. Refaktorert datasett-behandlingen.

--- b/main.py
+++ b/main.py
@@ -2,93 +2,91 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-x_data = ["hat ", "rat ", "cat ", "flat", "matt", "cap ", "son "]
-y_data = ["🎩", "🐀", "🐈", "🏠", "🚶", "🧢", "🧒"]
 
-characters = 27
-classes = len(y_data)
-characters_per_word = 4
+class Dataset:
+    def __init__(self, data):
+        self.data = np.array(data)
+        self.unique_chars = np.unique(list(''.join(self.data[:, 0])))
+        self.char_to_index = dict()
+        for element in self.unique_chars:
+            self.char_to_index[element] = len(self.char_to_index)
 
-char_encodings = np.eye(characters)
+    def encode_char(self, char):
+        return np.eye(len(self.unique_chars))[self.char_to_index[char]]
 
+    def encode_words(self):
+        one_hots = []
+        for word in self.data[:, 0]:
+            one_hot = []
+            for char in word:
+                one_hot.append(self.encode_char(char))
+            one_hots.append(one_hot)
+        return one_hots
 
-def string_to_array(string):
-    if len(string) != characters_per_word:
-        raise ValueError("Length of string must be 4")
-    char_array = list(string)
-    enc = np.zeros((len(char_array), characters))
-    for c_i in range(len(char_array)):
-        if char_array[c_i] == ' ':
-            enc[c_i] = np.array(char_encodings[0])
-        else:
-            enc[c_i] = np.array(char_encodings[ord(char_array[c_i]) - 96])
-    return enc
-
-
-def index_to_char(i):
-    if i == 0:
-        return ' '
-    else:
-        return chr(i + 96)
+    def y_to_word(self, one_hot):
+        return self.data[one_hot.argmax(), 1]
 
 
-amount_x_data = len(x_data)
-x_train_values = np.zeros((amount_x_data, characters_per_word, characters))
-for x_i in range(amount_x_data):
-    x_train_values[x_i] = string_to_array(x_data[x_i])
-
-y_train_values = np.eye(7)
-
-x_train = torch.tensor(x_train_values, requires_grad=True, dtype=torch.float)
-y_train = torch.tensor(y_train_values, dtype=torch.float)
-
-x_test_data = ['rat ', 'rt  ']
-x_test_values = np.zeros((len(x_test_data), characters_per_word, characters))
-for i in range(len(x_test_data)):
-    x_test_values[i] = string_to_array(x_test_data[i])
-
-x_test = torch.tensor(x_test_values, dtype=torch.float)
+dataset = Dataset([['hat ', '🎩'],
+                   ['rat ', '🐀'],
+                   ['cat ', '🐈'],
+                   ['flat', '🏢'],
+                   ['matt', '👨'],
+                   ['cap ', '🧢'],
+                   ['son ', '👦']])
+x_train = torch.tensor(dataset.encode_words(), dtype=torch.float).transpose(0, 1)
+y_train = torch.tensor(np.eye(np.shape(x_train)[1]), dtype=torch.float)
 
 
-class LSTM_ManyOne(nn.Module):
-    def __init__(self, encoding_size, label_length):
-        self.inner_size = 128
-        super(LSTM_ManyOne, self).__init__()
-        self.lstm = nn.LSTM(encoding_size, self.inner_size)
-        self.dense = nn.Linear(self.inner_size, label_length)
+class LSTMManyOne(nn.Module):
+    def __init__(self, encoding_size, label_size):
+        self.state_size = 128
+        super(LSTMManyOne, self).__init__()
+        self.lstm = nn.LSTM(encoding_size, self.state_size)
+        self.dense = nn.Linear(self.state_size, label_size)
+
+    def reset(self, batch_size):
+        zero_state = torch.zeros(1, batch_size, self.state_size)
+        self.hidden_state = zero_state
+        self.cell_state = zero_state
 
     def logits(self, x):
-        print(x.shape)
-        out, _ = self.lstm(x)
-        print(out[-1].shape)
-        return self.dense(out[-1].reshape(-1, self.inner_size))
+        out, (self.hidden_state, self.cell_state) = self.lstm(x, (self.hidden_state, self.cell_state))
+        return self.dense(out[-1].reshape(-1, self.state_size))
 
     def forward(self, x):
         return torch.softmax(self.logits(x), dim=1)
 
     def loss(self, x, y):
-        x = self.logits(x)
-        y = y.argmax(1)
-        print(x.shape)
-        print(y.shape)
-        return nn.functional.cross_entropy(x, y)
+        return nn.functional.cross_entropy(self.logits(x), y.argmax(1))
 
 
-model = LSTM_ManyOne(characters, 7)
+model = LSTMManyOne(len(dataset.unique_chars), np.shape(y_train)[1])
+
+
+def generate(text):
+    model.reset(1)
+    gen_x = []
+    for char in text:
+        gen_x.append(dataset.encode_char(char))
+    y = model.forward(torch.tensor(gen_x, dtype=torch.float).detach().reshape(4, 1, -1))
+    return dataset.y_to_word(y)
+
 
 lr = 0.05
 epochs = 500
 optimizer = torch.optim.RMSprop(model.parameters(), lr)
 
 for epoch in range(epochs):
+    model.reset(x_train.size(1))
     loss = model.loss(x_train, y_train)
     loss.backward()
     optimizer.step()
     optimizer.zero_grad()
-    if epoch % 100 == 99:
-        print("Epoch %i: Loss: %s" % (epoch, loss.item()))
-
-print("----------------")
-
-y_values = model.forward(x_test).detach().numpy()
+    if epoch % 100 == 9:
+        print("Epoch %i: Loss: %s | 'rats'=>%s, 'rt  '=>%s, 'mt  '=>%s" % (epoch,
+                                                                           loss.item(),
+                                                                           generate('rats'),
+                                                                           generate('rt  '),
+                                                                           generate('mt  ')))
 
